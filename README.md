812d2d394ad7216bb40e925b9615f419282ab7c1
Jamie Spittal <lastmanout@gmail.com>
2020-05-28 11:41:03 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Vue HMR Slot Bug
+
+To run
+```
+yarn
+yarn dev
+```
+
+Then open `App.vue` and edit any content inside either `SimpleSlot` component and notice the HMR doesn't work.
---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Slot HMR Bug Reproduction</h1>
+  <SimpleSlot>
+    Outside Slot
+    <SimpleSlot>
+      Inside Slot
+    </SimpleSlot>
+  </SimpleSlot>
+</template>
+
+<script>
+import SimpleSlot from './components/SimpleSlot.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SimpleSlot
+  }
+}
+</script>
